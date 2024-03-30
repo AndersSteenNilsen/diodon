d99082ff6e407621e22b8110c3cfdac084bb9059
Anders Steen <asteennilsen@gmail.com>
2024-03-30 16:14:27 +0100

    better organized

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,33 @@ use util::from_biguint_dig_to_biguint;
 
 mod util;
 
+pub fn conservative_params() -> Params {
+    Params {
+        m: 4_000usize,
+        l: 4_000usize,
+        time_complexity: 2048usize,
+        key_bit_size: 2048usize,
+        hash_bytes_size: 16usize,
+    }
+}
+
+pub fn fast_params() -> Params {
+    Params {
+        m: 8_000_000usize,
+        l: 20_000usize,
+        time_complexity: 1usize,
+        key_bit_size: 1024,
+        hash_bytes_size: 16usize,
+    }
+}
+
 // Params for Dioidon.
 // TODO: Add hash function as a param.
 pub struct Params {
     m: usize,
     l: usize,
     time_complexity: usize,
+    pub key_bit_size: usize,
     hash_bytes_size: usize,
 }
 
@@ -22,46 +43,63 @@ pub struct PublicKey {
     rsa_n: BigUint,
 }
 
+#[derive(Debug)]
 pub struct PrivateKey {
     rsa_p: BigUint,
     rsa_q: BigUint,
-    rsa_n: BigUint,
-    rsa_phi_n: BigUint,
 }
 
-pub fn generate_2048_bit_keys() -> (PrivateKey, PublicKey) {
+pub fn generate_keys(bit_size: usize) -> (PrivateKey, PublicKey) {
     let mut rng = rand::thread_rng();
-    let key = RsaPrivateKey::new(&mut rng, 2048).unwrap();
-    let rsa_phi_n = from_biguint_dig_to_biguint(&(&key.primes()[0] * &key.primes()[1]));
+    let key = RsaPrivateKey::new(&mut rng, bit_size).unwrap();
     (
         PrivateKey {
             rsa_p: from_biguint_dig_to_biguint(&key.primes()[0]),
             rsa_q: from_biguint_dig_to_biguint(&key.primes()[1]),
-            rsa_n: from_biguint_dig_to_biguint(&key.n()),
-            rsa_phi_n: rsa_phi_n,
         },
         PublicKey {
-            rsa_n: from_biguint_dig_to_biguint(&key.n()),
+            rsa_n: from_biguint_dig_to_biguint(key.n()),
         },
     )
 }
 
-pub fn hard() {}
+pub fn privileged(cipher: &[u8], private_key: PrivateKey, params: &Params) -> Vec<u8> {
+    diodon_privileged(
+        cipher,
+        &private_key.rsa_p,
+        &private_key.rsa_q,
+        params.m,
+        params.l,
+        params.time_complexity,
+        params.hash_bytes_size,
+    )
+}
+
+pub fn non_priviledged(cipher: &[u8], public_key: PublicKey, params: &Params) -> Vec<u8> {
+    diodon_non_privileged(
+        cipher,
+        &public_key.rsa_n,
+        params.m,
+        params.l,
+        params.time_complexity,
+        params.hash_bytes_size,
+    )
+}
 
 fn diodon_non_privileged(
-    x_cipher_block: usize,
-    rsa_n: BigUint,
+    cipher: &[u8],
+    rsa_n: &BigUint,
     m: usize,
     l: usize,
     time_complexity: usize,
     hash_bytes_size: usize,
 ) -> Vec<u8> {
-    let x = BigUint::from(x_cipher_block);
+    let x = BigUint::from_bytes_le(cipher);
     let mut v: Vec<BigUint> = Vec::<BigUint>::with_capacity(m);
     v.push(x);
     let start: Instant = Instant::now();
     for i in 1..m {
-        v.push(util::squaring(&v[i - 1], time_complexity, &rsa_n));
+        v.push(util::squaring(&v[i - 1], time_complexity, rsa_n));
     }
     println!("Time elapsed in vector_pushing() is: {:?}", start.elapsed());
     let start: Instant = Instant::now();
@@ -76,12 +114,11 @@ fn diodon_non_privileged(
         s_bytes = blake3::hash(&s_bytes).as_bytes().to_vec();
     }
     println!("Time elapsed in L() is: {:?}", start.elapsed());
-    println!("{:?}", s_bytes);
     s_bytes[s_bytes.len() - hash_bytes_size..].to_vec()
 }
 
 fn diodon_privileged(
-    x_cipher_block: usize,
+    cipher: &[u8],
     rsa_p: &BigUint,
     rsa_q: &BigUint,
     m: usize,
@@ -89,7 +126,7 @@ fn diodon_privileged(
     time_complexity: usize,
     hash_bytes_size: usize,
 ) -> Vec<u8> {
-    let x: BigUint = x_cipher_block.into();
+    let x: BigUint = BigUint::from_bytes_le(cipher); // x_cipher_block.into();
     let exponent = BigUint::from((m - 1) * time_complexity);
     let phi_n = (rsa_p - 1usize) * (rsa_q - 1usize);
     let n = rsa_p * rsa_q;
@@ -120,16 +157,16 @@ mod tests {
     use super::*;
     #[test]
     fn diodon_hard_equals_privileged() {
-        let q = BigUint::parse_bytes(b"12772322319733548247851901381850054224408980869676616358291561606873489416423155106454795516367791954119113161475136097310823566024399906461641393526506223", 10).unwrap();
-        let p = BigUint::parse_bytes(b"13270159569298364102590828989123999927823242049974571921817075346300096102090311023718167382683031794589299932545623449542461777499628836970633616840367291", 10).unwrap();
+        let q = BigUint::parse_bytes(b"156968538006454153552154918080725604889881249793955878307143380021187327061208436911326291489110631759094141457413830907419424449436820339912357318330772975266767639408357800395570329480229784588927678308046854933619619353739869481851305550105159797781519661688972486987059268302270270193066850179922150396937", 10).unwrap();
+        let p = BigUint::parse_bytes(b"138154375592216506317406833347480794151135359156985136226058739441317232969359215666883048156303905347167585362049791808140291742161041436935911667281520624494327845377621238269655349853869338664638587537443448410326271340862757680139090487799781391799061369299343370912316229801139344805020703321280063177759", 10).unwrap();
         let rsa_n = &p * &q;
-        let cipher: usize = 21;
-        let m = 4000;
-        let l = 4000;
+        let cipher: &[u8] = b"Hello Diodon";
+        let m = 4_000;
+        let l = 4_000;
         let u_bytes: usize = 16; //128 bits
         let time_complexity = 2048;
-        let hashed_hard = diodon_non_privileged(cipher, rsa_n, m, l, time_complexity, u_bytes);
         let hashed_easy = diodon_privileged(cipher, &p, &q, m, l, time_complexity, u_bytes);
+        let hashed_hard = diodon_non_privileged(cipher, &rsa_n, m, l, time_complexity, u_bytes);
         println!(
             "hashed_hard={:?}, hashed_easy={:?}",
             hashed_hard, hashed_easy
--- tests/main_test.rs
+++ tests/main_test.rs
@@ -1,8 +1,25 @@
-/*
+use diodon::Params;
+
 #[test]
-fn fast_hard_diodon_hash(){
-    params = diodon::fast_params;
-    let cipther = b"Hello Diodon";
-    let hash = diodon::hard(fast_params);
+fn fast_diodon_hash() {
+    let fast_params: Params = diodon::fast_params();
+    let cipher: &[u8; 12] = b"Hello Diodon";
+    let (private_key, public_key) = diodon::generate_keys(fast_params.key_bit_size);
+    let hash_priviledgeged: Vec<u8> = diodon::privileged(cipher, private_key, &fast_params);
+    let hash_non_privileged: Vec<u8> = diodon::non_priviledged(cipher, public_key, &fast_params);
+    assert_eq!(hash_priviledgeged, hash_non_privileged);
+    println!("{:?}\n{:?}", hash_non_privileged, hash_priviledgeged);
 }
-*/
+
+#[test]
+fn conservative_diodon_hash() {
+    let conservative_params: Params = diodon::conservative_params();
+    let cipher: &[u8; 12] = b"Hello Diodon";
+    let (private_key, public_key) = diodon::generate_keys(conservative_params.key_bit_size);
+    println!("{:?}", private_key);
+    let hash_priviledgeged: Vec<u8> = diodon::privileged(cipher, private_key, &conservative_params);
+    let hash_non_privileged: Vec<u8> =
+        diodon::non_priviledged(cipher, public_key, &conservative_params);
+    assert_eq!(hash_priviledgeged, hash_non_privileged);
+    println!("{:?}\n{:?}", hash_non_privileged, hash_priviledgeged);
+}
